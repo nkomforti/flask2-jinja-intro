caf76f58a711dd4921488fbd0fc139d6b95da890
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-18 12:34:12 -0700

    Modified compliment.html, created /game and /madlib, created goodbye.html, madlib.html, and game.html templates.

---
+++ madlibs.py
@@ -0,0 +1,76 @@
+"""A madlib game that compliments its users."""
+
+from random import choice
+
+from flask import Flask, render_template, request
+
+# "__name__" is a special Python variable for the name of the current module.
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful',
+    'smashing', 'lovely',
+]
+
+
+@app.route('/')
+def start_here():
+    """Display homepage."""
+
+    return "Hi! This is the home page."
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello to user."""
+
+    return render_template("hello.html")
+
+
+@app.route('/greet')
+def greet_person():
+    """Greet user with compliment."""
+
+    player = request.args.get("person")
+
+    compliment = choice(AWESOMENESS)
+
+    return render_template("compliment.html",
+                           person=player,
+                           compliment=compliment)
+
+
+@app.route('/game')
+def show_madlib_form():
+    """"""
+
+    game_choice = request.args.get("game_choice")
+    player = request.args.get("person")
+
+    if game_choice == 'no':
+        return render_template("goodbye.html", player=player)
+
+    else:
+        return render_template("game.html")
+
+
+@app.route('/madlib')
+def show_madlib():
+    """ """
+
+    person = request.args.get("person")
+    noun = request.args.get("noun")
+    color = request.args.get("color")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", person=person, noun=noun,
+                           color=color, adjective=adjective)
+
+
+if __name__ == '__main__':
+    # Setting debug=True gives us error messages in the browser and also
+    # "reloads" our web app if we change the code.
+
+    app.run(debug=True)
